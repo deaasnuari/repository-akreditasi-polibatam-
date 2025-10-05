d28b89d6caa1d2405f9639c211a83b86641f3e64
Deaasnuari <deaasnuari@gmail.com>
2025-10-05 16:22:27 +0700

    ganti port 3000

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: "http://localhost:3001",   // izinkan hanya frontend kamu
+  origin: "http://localhost:3000",   // izinkan hanya frontend kamu
   methods: ["GET", "POST"],
   credentials: true
 }));
