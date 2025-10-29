e0215739e96035f0df46bd5f233877e7210828ff
Syahnaz Dwi <syhnzdwi@gmail.com>
2025-10-29 14:00:13 +0700

    perubahan sedikit pada layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
 
         {/* Isi halaman */}
-        <main className="flex-1 mt-20">
+        <main className="flex-1 pt-16">
           {children}
         </main>
 
